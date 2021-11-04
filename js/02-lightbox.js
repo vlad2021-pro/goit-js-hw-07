3e3d13a876ab86b6a96abb30a8cf83c1c3dc9d8c
vlad2021-pro <84739712+vlad2021-pro@users.noreply.github.com>
2021-11-04 18:49:27 +0300

    Fix lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -20,49 +20,11 @@ const newGallery = galleryItems.map(element => {
 
 gallaryNode.insertAdjacentHTML('beforeend', newGallery)
   
-document.querySelectorAll('.gallery__image').forEach(img => {
-  img.onclick = onImgClick
-  
-});
-
-const instance = basicLightbox.create(
-  `<img class="modal-img" src="">`,
-  {
-    onShow: instance => {
-      window.addEventListener('keydown', onEscClick);
-    },
-  },
-  {
-    onClose: instance => {
-      window.removeEventListener('keydown', onEscClick);
-    },
-  },
-);
-function onImgClick(evt) {
-  evt.preventDefault();
-  
-    if (evt.target.nodeName !== 'IMG') {
-      return;
-
-    }
-    instance.element().querySelector('.modal-img').src =
-      evt.target.dataset.source;
-    instance.show();
-}
-   
-  function onEscClick(evt) {
-    if (evt.key === 'Escape') {
-      instance.close();
-      return;
-    }
-  }
-
-
 let gallery = new SimpleLightbox('.gallery a',
   {
-captionDelay: 'alt',
+captionDelay: '250',
 captionPosition: 'bottom',
-captionsData: '250'
+captionsData: 'alt'
   });
 
 
